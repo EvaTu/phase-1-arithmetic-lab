111caf26bb3e060e2f80cadb512c44cecda0f9c0
EvaTu <evatuw@gmail.com>
2021-08-21 22:26:29 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1*num2;
+
+const random = Math.floor(Math.random()*100)+1
+
+const num3 = 104;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(4,17,20,3,9)
